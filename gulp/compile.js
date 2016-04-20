ca5c9dc6081408a811e83da0eca30c96a3be78e3
vol4tim <vol4tim@gmail.com>
2016-04-20 11:45:20 +0400

    compile ignore gulp

--- gulp/compile.js
+++ gulp/compile.js
@@ -2,7 +2,7 @@
 var exec = require('child_process').exec;
 
 module.exports = (gulp, plugins, config) => () => {
-    return exec("electron-packager . a3 --platform=win32 --arch=x64 --version=0.37.5 --app-version=0.1.0 --out=release --ignore=\"node_modules/(electron-packager|electron-prebuilt)\" --ignore=\"blockchains/\" --ignore=\"config/\" --ignore=\"gb/\" --ignore=\"nodes/\" --ignore=\"release/\" --asar=true --prune", function(err) {
+    return exec("electron-packager . a3 --platform=win32 --arch=x64 --version=0.37.5 --app-version=0.1.0 --out=release --ignore=\"node_modules/(electron-packager|electron-prebuilt)\" --ignore=\"blockchains/\" --ignore=\"config/\" --ignore=\"gb/\" --ignore=\"nodes/\" --ignore=\"release/\" --ignore=\"gulp/\" --asar=true --prune", function(err) {
         console.log(err);
       /*if (err) return cb(err);
       cb();*/
